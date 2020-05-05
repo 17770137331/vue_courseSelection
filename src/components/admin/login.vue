580f0a40d3293961148ee92116172fcad0d4e0d6
yjmayun <2784161734@qq.com>
2020-05-05 14:19:24 +0800

    博客前端

---
+++ src/components/admin/login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="adminLogin">
+111xsaxasxaxsacas <el-button type='info'>111</el-button>
+<el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="less" scoped>
+.adminLogin{
+    background: red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '../src/assets/globel.css'
+import '../src/plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
